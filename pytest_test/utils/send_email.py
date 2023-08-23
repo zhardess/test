287c0366400d7c8e929bf649892f82ac39839c00
zhardess <59010650+zhardess@users.noreply.github.com>
2023-08-23 16:51:29 +0800

    Update send_email.py

--- pytest_test/utils/send_email.py
+++ pytest_test/utils/send_email.py
@@ -6,10 +6,10 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from email.header import Header
 
-sender = '564197253@qq.com'
-receivers = ['zhangzheazy@163.com', '3462208676@qq.com']
+sender = 'xxx@qq.com'
+receivers = ['xxx@163.com', 'xxx@qq.com']
 message = MIMEMultipart()
-message['From'] = Header("a 564197253@qq.com")
+message['From'] = Header("a xxx@qq.com")
 message['To'] =  Header("111", 'utf-8')
 message['Subject'] = Header('带附件的邮件测试', 'utf-8')
 message.attach(MIMEText('这是用Python编写的邮件发送程序……', 'plain', 'utf-8'))
@@ -23,7 +23,7 @@ att2.add_header('content-disposition', 'attachment', filename='pow_report.html')
 message.attach(att2)
 smtp = smtplib.SMTP()
 smtp.connect('smtp.qq.com')
-smtp.login('564197253@qq.com', 'gigkpayfonfgbchc')
+smtp.login('xxx@qq.com', 'xxx')
 smtp.sendmail(sender, receivers, message.as_string())
 print ("邮件发送成功！！！")
 smtp.quit()
